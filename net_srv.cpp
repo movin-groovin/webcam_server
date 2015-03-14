186f2458a1978216ec8c938698cc4b1aec164345
rainbow <rainbow@dhcppc1.one>
2015-03-14 23:19:48 +0000

    makefile

--- net_srv.cpp
+++ net_srv.cpp
@@ -2,8 +2,6 @@
 // http://habrahabr.ru/post/192284/
 // http://www.bogotobogo.com/cplusplus/Boost/boost_AsynchIO_asio_tcpip_socket_server_client_timer_bind_handler_multithreading_synchronizing_network_D.php
 
-// g++ -std=c++11 -I/usr/include/opencv2 -lopencv_highgui -lopencv_core -lopencv_imgproc net_srv.cpp -o net_srv
-
 
 
 #include "network_header.hpp"
