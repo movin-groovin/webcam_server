6aaa452e914de13f677ffa6d1431e7d056ba22b1
yarr <yarr@debian>
2015-04-17 18:39:25 +0000

    begin writing of unittests

---
+++ main_client.cpp
@@ -0,0 +1,7 @@
+
+int EntryPointClient(int argc, char *argv[]);
+
+
+int main(int argc, char *argv[]) {
+    return EntryPointClient(argc, argv);
+}
---
+++ main_server.cpp
@@ -0,0 +1,7 @@
+
+int EntryPointServer(int argc, char *argv[]);
+
+
+int main(int argc, char *argv[]) {
+    return EntryPointServer(argc, argv);
+}
